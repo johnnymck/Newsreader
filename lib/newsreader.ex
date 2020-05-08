a537dc61b82e0c7788281363787cf2a696807aa6
Johnny McKenzie <johnny@Johnnys-iMac.local>
2020-05-09 00:26:32 +0100

    read the manual. Did everything in one line haha!

--- lib/newsreader.ex
+++ lib/newsreader.ex
@@ -3,8 +3,7 @@ defmodule Newsreader do
   Documentation for Newsreader.
   """
 
-  def sources do 
-      [
+  @sources [
       "https://www.theguardian.com/world/rss",
       "https://www.heraldscotland.com/news/homenews/rss/",
       "http://www.independent.co.uk/news/world/rss",
@@ -19,46 +18,16 @@ defmodule Newsreader do
       "https://www.derstandard.at/rss",
       "http://feeds.washingtonpost.com/rss/world"
     ]
-  end
 
-  def proc_get_feed_body({parent, url}) do
-    try do 
-      {:ok, response} = Mojito.get(url)
-      {:ok, rss} = FastRSS.parse(response.body)
-      send(parent, {:ok, rss})
-    rescue 
-      e in MatchError ->
-        send(parent, Tuple.append(e.term, url))
-    end
-  end
-
-  def fetch_feeds_batch(sources) do
-    Enum.map(sources, fn i -> 
-      spawn(Newsreader, :proc_get_feed_body, [{self(), i}])
-    end)
-  end
-
-  def aggrigate_feeds(feeds_list \\ []) do
-    receive do
-      {:ok, response} ->
-        aggrigate_feeds([{:ok, response} | feeds_list])
-      {:error, error, url} ->
-        aggrigate_feeds([{:error, error, url}|feeds_list])
-      {:error, error} ->
-        aggrigate_feeds([{:error, :xmlhingy, error}|feeds_list])
-      after 2_500 -> 
-        {:ok, feeds_list}
-    end
+  def get_atom_feeds() do
+    @sources
+    |> Task.async_stream(&Mojito.get/1)
+    |> Enum.into([], fn {:ok, res} -> res end)
   end
 
   def run do
     IO.puts("fetching news sources for you...")
-    fetch_feeds_batch(sources)
-    IO.puts("got them. Processing now...")
-    feeds = aggrigate_feeds
-    # |> transform_add_paper_metadata_to_item
-    # |> order_story_by_timestamp
-    # |> beautify_output
+    feeds = get_atom_feeds()
     IO.inspect(feeds)
   end
 end
